8755bcfcd47e013d2367dea4f4f8815abf428ac8
son-inchang <3721cha21.11@gmail.com>
2025-03-04 14:23:16 +0900

    feat: Set up Geminiboard Spring Boot project with core dependencies

---
+++ src/main/java/com/sonicplayground/geminiboard/GeminiboardApplication.java
@@ -0,0 +1,13 @@
+package com.sonicplayground.geminiboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonicplayground/geminiboard/GeminiboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonicplayground.geminiboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
