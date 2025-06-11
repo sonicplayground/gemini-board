7fac563a2f7f421bca9839bb680da40582fb84a7
son-inchang <3721cha21.11@gmail.com>
2025-06-11 10:46:13 +0900

    feat: PATCH 추가

--- src/main/java/com/sonicplayground/geminiboard/infrastructure/web/WebConfig.java
+++ src/main/java/com/sonicplayground/geminiboard/infrastructure/web/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000") // Nuxt 개발 서버 주소
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
     }
