51524fe6a9a07bdd01bf34a93aca80dc7cf22475
son-inchang <3721cha21.11@gmail.com>
2025-06-11 10:47:32 +0900

    feat: auth exception handler 추가

--- src/main/java/com/sonicplayground/geminiboard/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sonicplayground/geminiboard/common/exception/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,6 +29,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<Map<String, String>> handleAuthorizationDeniedException(AuthorizationDeniedException e) {
+            String message = e.getMessage();
+
+            if (!StringUtils.hasText(message)) {
+                message = "authorization denied";
+            }
+
+            log.error("AuthorizationDeniedException: {}", message);
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("message", message);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleException(Exception e) {
